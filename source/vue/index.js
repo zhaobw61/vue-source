dabdef89644c3216d5f555aeefebb69451719aab
zhaobw <1964471757@qq.com>
2020-09-25 00:38:46 +0800

    完成模板渲染

--- source/vue/index.js
+++ source/vue/index.js
@@ -1,5 +1,6 @@
 import { initState } from './observe/index'
 import Watcher from './observe/watcher';
+
 function Vue(options) {
     this._init(options);
 }
@@ -20,12 +21,45 @@ function query(el){
     }
     return el;
 }
+// ?: 匹配不捕获
+const defaultRE = /\{\{((?:.|\r?\n)+?)\}\}/g;
+const util = {
+    getValue(vm, expr){
+        let keys = expr.split('.');
+        return keys.reduce((memo,current) => {
+            memo = memo[current];
+            return memo;
+        },vm)
+    },
+    compilerText(node, vm) {
+        node.textContent = node.textContent.replace(defaultRE, function(...args){
+            return util.getValue(vm, args[1]);
+        });
+    }
+}
+function compiler(node, vm){
+    let childNodes = node.childNodes;
+    [...childNodes].forEach(child => {
+        if(child.nodeType == 1) { // 1 代表元素  3表示文本
+            compiler(child, vm);
+        }else if(child.nodeType == 3) {
+            util.compilerText(child, vm);
+        }
+    })
+}
 Vue.prototype._update = function() {
     // 用用户传入的数据 去更新数据
     let vm = this;
     let el = vm.$el;
     // 要循环这个元素 将里面的内容 换成我们的数据
-    
+    let node = document.createDocumentFragment();
+    let firstChild;
+    while(firstChild = el.firstChild) {
+        node.appendChild(firstChild); // appendChild 是具有移动的功能的
+    }
+    // 对文本进行替换
+    compiler(node, vm);
+    el.appendChild(node);
 }
 // 渲染页面 将组件进行挂载
 Vue.prototype.$mount = function() {
